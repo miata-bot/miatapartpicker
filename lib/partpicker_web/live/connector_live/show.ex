5a36c708337de1ba45c9042d4512de8b22215251
Connor Rigby <connorr@hey.com>
2021-04-14 10:43:04 -0700

    Cleanup accidental screenshots

--- lib/partpicker_web/live/connector_live/show.ex
+++ lib/partpicker_web/live/connector_live/show.ex
@@ -1,6 +1,74 @@
 defmodule PartpickerWeb.ConnectorLive.Show do
   use PartpickerWeb, :live_view
 
+  defmodule MyWorker do
+    @moduledoc """
+    Consider you have a process, off doing some work. We will call it `MyWorker`.
+    This worker will do one simple task, increment a count.
+
+    You start a worker with:
+
+        iex> {:ok, worker} = MyWorker.start_link(0)
+        {:ok, #PID<0.185.0>}
+
+    Now that worker will start incrementing the count at random intervals. We can
+    check the state of the worker using the `:sys` module:
+
+        iex> :sys.get_state(worker)
+        %{caller: nil, value: 34}
+
+    Now we want the worker to keep doing it's work; we may also want the current process (not the worker)
+    to be blocked until a certain amount of work is done. The function `block_until_eq/2` will
+    block the current process, `(self())` until the work count reaches the inputted value.
+
+      iex> MyWorker.block_until_eq(worker, 50)
+      # process will be blocked until the work is up..
+      {:ok, 50}
+
+    While the calling processes is blocked, the Worker is still processing
+    """
+    use GenServer
+
+    def start_link(initial_value \\ 0), do: GenServer.start_link(__MODULE__, initial_value)
+
+    def block_until_eq(worker, input, timeout \\ 5_000) do
+      GenServer.call(worker, {:block_until_eq, input}, timeout)
+    end
+
+    @impl GenServer
+    def init(initial_value) do
+      send self(), :work
+      {:ok, %{value: initial_value, caller: nil}}
+    end
+
+    @impl GenServer
+    def handle_info(:work, state) do
+      state = do_work(state)
+      send self(), :work
+      {:noreply, state}
+    end
+
+    @impl GenServer
+    def handle_call({:block_until_eq, input}, caller, state) do
+      {:noreply, %{state | caller: {caller, input}}}
+    end
+
+    # if the work has increased `value` such that the value is now greater than block,
+    # we return the `handle_call` to the calling process
+    def do_work(%{value: value, caller: {caller, block}} = state) when value > block do
+      _ = GenServer.reply(caller, {:ok, value})
+      %{state | value: 0, caller: nil}
+    end
+
+    # otherwise, continue working
+    def do_work(%{value: value} = state) do
+      # pretend work going on here
+      Process.sleep(System.unique_integer([:positive]))
+      new_value = value + 1
+      %{state | value: new_value}
+    end
+  end
+
   alias Partpicker.Library
 
   @impl true
